823a4a03cee7daafd5772a7bafc8bbd8114ad41a
avalonv <avalonvales@protonmail.com>
2019-11-13 20:07:12 -0300

    invert write order, write most recent measurement last

--- log_csv.py
+++ log_csv.py
@@ -5,18 +5,16 @@ def read_rows():
     rows = []
     with open('history.csv', 'r', newline='') as csvfile:
         myreader = csv.reader(csvfile, delimiter=',')
-        i = 1
         for row in myreader:
-            rows.insert(i, row)
-            i += 1
+            rows.append(row)
     return rows
 
 
 def write_rows(timestamp, time_str, temp, max_rows=10):
     rows = read_rows()
-    rows.insert(0, [timestamp, time_str, temp])  # insert most recent reading
+    rows.append([timestamp, time_str, temp])  # insert most recent reading
     if len(rows) > max_rows:
-        rows.pop(-1)  # remove oldest reading
+        rows.pop(0)  # remove oldest reading
     with open('history.csv', 'w', newline='') as csvfile:
         mywriter = csv.writer(csvfile, delimiter=',')
         mywriter.writerow(["temestamp", "time_str", "temp"])  # write header
