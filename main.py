13c574dc8293bc2cf0419f5962268cfb91017563
avalonv <avalonvales@protonmail.com>
2019-11-16 06:30:48 -0300

    only warn if high_temps exceed warn_threshold

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ warn_interval = 600 # 10 minutes
 refresh_time = 3
 locale_str = 'pt_BR.utf8'
 history_limit = 3600 # * 3 = keep around 3 hours of history
+high_temps = []
+warn_threshold = 60 # send a warning if the temperature stays high for a minute
+
 
 try:
     locale.setlocale(locale.LC_ALL, locale_str)
@@ -31,14 +34,22 @@ def send_email(temp):
 
 
 def log_temperature(temp):
-    global last_warn
+    global last_warn, high_temps
     time_now = int(time.time())  # unix timestamp
     date_str = time.strftime("%Y-%m-%d %H:%M:%S").strip(' ')  # human readable date
     write_csv(time_now, date_str, temp, history_limit)
     if current_temp > max_temp:
-        if (last_warn + warn_interval) < time_now:
-            send_email(temp)
-            last_warn = time_now
+        high_temps.append(time_now)
+        # check if the temperature has remained above the max_temp limit
+        # for longer than a certain amount of time
+        if high_temps[0] - high_temps[-1] > warn_threshold:
+            # check if a warning was already sent recently
+            if (last_warn + warn_interval) < time_now:
+                send_email(temp)
+                last_warn = time_now
+        else:
+            # reset high temps
+            high_temps = []
 
 
 while True:
