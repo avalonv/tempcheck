765b988002f9e3a8d45b65a4d64e3dcb01c8c606
avalonv <avalonvales@protonmail.com>
2019-11-24 20:11:54 -0300

    add update_graph()

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
+import threading
 from os import system
 from read_temperature import read_temp
 from csv_logger import write_csv
 from plot_graph import draw_line_graph
+from write_html import update_webpage
 import time
 import locale
 
+locale_str = 'pt_BR.utf8'
 max_temp = 25
 last_warn = 0
-warn_interval = 600 # 10 minutes
+last_update = 0
 refresh_time = 3
-locale_str = 'pt_BR.utf8'
 history_limit = 3600 # * 3 = keep around 3 hours of history
 high_temps = []
 warn_threshold = 60 # send a warning if the temperature stays high for a minute
@@ -21,7 +23,7 @@ try:
     locale.setlocale(locale.LC_ALL, locale_str)
 except (locale.Error):
     print('unsupported locale')
-    print(f"run 'sudo dpkg-reconfigure locales' to configure {locale_str} locale")
+    print(f"run `sudo dpkg-reconfigure locales` to configure {locale_str} locale")
     exit(1)
 
 
@@ -34,11 +36,22 @@ def send_email(temp):
     # system(f"echo {body} | s-nail -s '{subject}' '{recipient}'")
 
 
+def update_graph(update_interval=300):
+    global last_update
+    graph_thread = threading.Thread(target=draw_line_graph)
+    if (last_update + update_interval) < time_now:
+        if not graph_thread.isAlive():
+            graph_thread.start()
+            last_update = time_now
+    update_webpage()
+
+
 while True:
     temp = read_temp()
     time_now = int(time.time())
     date_str = time.strftime("%Y-%m-%d %H:%M:%S").strip(' ')
     write_csv(time_now, date_str, temp, history_limit)
+    update_graph()
     if temp > max_temp:
         high_temps.append(time_now)
         # check if the temperature has remained above the max_temp limit
